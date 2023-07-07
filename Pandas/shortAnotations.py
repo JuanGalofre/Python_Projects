00fae57fdccc7790664908ac5f8243cd67fb7f2f
JuanGalofre <108921687+JuanGalofre@users.noreply.github.com>
2023-07-06 19:46:43 -0500

    Create shortAnotations.py

---
+++ Pandas/shortAnotations.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+df=pd.read_csv("LocatDB.csv")
+
+"""
+
+df.head()-> Para mirar las primeras 5 filas
+df.tail() -> Para mirar las ultimas 5 filas
+
+
+for c in df.columns():
+    print(c)
+-> Va a imprimir todos los nombres de las columnas. Al recorrerse con un ciclo se deduce que puede ser una, lista, pero para recorrerla como tal se utiliza
+c=list(df.columns)
+
+-> Para cambiar el orden del dataframe, lo que se puede hacer es cambiar el orden de esa lista. 
+   En este caso la columnas son c, pero simplemente es la lista donde se tiene los nombres de las columnas
+df=df.reindex(columns=c) 
+
+-> Para eliminar algunas columnas del dataframe, se crea un nuevo , o la lista donde se tiene la lista de nombres de las columnas
+df=df[c]
+
+-> Para modificar el valor de una columna, en este caso un string.astype castea
+df["nombreColumna"]=df["nombreColumna"].str.replace(r"loQueQueremosReemplazar","LoQueVaASerReemplazado").astype(float)
+
+-> Concatenar DF,se concatenan con concat. Para mirar la longitud y comprobar que se concatenaron, se puede usar len(df.index), porque el index siempre va a tener algo. Aunque eso tambien 
+   contaria lineas vacias en el caso de un excel. Yo me imagino que para poder concatenar, deberian tener las mismas columnas con los mismos nombres. Pero para eso se pueden editar ya sean 
+   los nombres o el orden.
+df2 = pd.read_csv("LocatDB2.csv") 
+con_df=pd.concat([df1,df2]) 
+len(df1.index)
+
+"""
